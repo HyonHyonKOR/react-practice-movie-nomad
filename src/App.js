c0c4ca81be9e08cf60fc5d9cce6da8b62e74a1e1
HyonHyonKOR <khm3626@gmail.com>
2024-08-05 23:56:40 +0900

    feat:coin tracker

--- src/App.js
+++ src/App.js
@@ -1,35 +1,63 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import styles from "./App.module.css";
 
 function App() {
-  const [toDo, setToDo] = useState("");
-  const [toDos, setToDos] = useState([]);
+  const [coinName, setCoinName] = useState("");
+  const [coinPrice, setCoinPrice] = useState(0);
+  const [money, setMoney] = useState();
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+
   const onChange = (event) => {
-    setToDo(event.target.value);
+    setMoney(event.target.value);
   };
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if (toDo.trim() === "") {
-      return;
-    }
-    setToDos((currentArray) => [toDo, ...currentArray]);
-    setToDo("");
+
+  const onSelect = (event) => {
+    const arr = event.target.value.split(",");
+    const [selectedCoinPrice, selectedCoinName] = arr;
+    setCoinPrice(selectedCoinPrice);
+    setCoinName(selectedCoinName);
   };
+
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response) => response.json())
+      .then((data) => {
+        setCoins(data);
+        setLoading(false);
+        setCoinPrice(data[0].quotes.USD.price);
+        setCoinName(data[0].name);
+      });
+  }, []);
+
   return (
     <div>
-      <h1>{toDos.length}</h1>
-      <form onSubmit={onSubmit}>
-        <input
-          onChange={onChange}
-          value={toDo}
-          type="text"
-          placeholder="Write your to do..."
-        />
-        <button>Add To Do</button>
-      </form>
-      <hr />
-      {toDos.map((item, index) => (
-        <li key={index}>{item}</li>
-      ))}
+      <h1>The Coins!</h1>
+      {loading ? (
+        <strong>Loading...</strong>
+      ) : (
+        <div>
+          <select onChange={onSelect}>
+            {coins.map((coin) => (
+              <option key={coin.id} value={[coin.quotes.USD.price, coin.name]}>
+                {coin.name} ({coin.symbol}) : $
+                {coin.quotes.USD.price.toFixed(2)} USD
+              </option>
+            ))}
+          </select>
+          <input
+            className={styles.input}
+            onChange={onChange}
+            value={money}
+            type="number"
+            placeholder="Enter your budget($)"
+          />
+
+          <h5>
+            You can buy {money ? Math.floor(money / coinPrice) : 0} {coinName}
+          </h5>
+        </div>
+      )}
     </div>
   );
 }

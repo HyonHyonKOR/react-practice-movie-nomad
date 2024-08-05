bc2348639ac22113c58766d16e27af391028f5b9
HyonHyon <khm3626@gmail.com>
2024-08-06 02:06:53 +0900

    feat:move app basic

--- src/App.js
+++ src/App.js
@@ -1,61 +1,43 @@
 import { useEffect, useState } from "react";
-import styles from "./App.module.css";
 
 function App() {
-  const [coinName, setCoinName] = useState("");
-  const [coinPrice, setCoinPrice] = useState(0);
-  const [money, setMoney] = useState();
   const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([]);
+  const [movies, setMovies] = useState([]);
 
-  const onChange = (event) => {
-    setMoney(event.target.value);
-  };
-
-  const onSelect = (event) => {
-    const arr = event.target.value.split(",");
-    const [selectedCoinPrice, selectedCoinName] = arr;
-    setCoinPrice(selectedCoinPrice);
-    setCoinName(selectedCoinName);
+  const getMovies = async () => {
+    const json = await (
+      await fetch(
+        "https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year"
+      )
+    ).json();
+    setMovies(json.data.movies);
+    setLoading(false);
   };
 
   useEffect(() => {
-    fetch("https://api.coinpaprika.com/v1/tickers")
-      .then((response) => response.json())
-      .then((data) => {
-        setCoins(data);
-        setLoading(false);
-        setCoinPrice(data[0].quotes.USD.price);
-        setCoinName(data[0].name);
-      });
+    getMovies();
   }, []);
 
   return (
     <div>
-      <h1>The Coins!</h1>
       {loading ? (
-        <strong>Loading...</strong>
+        <h1>loading...</h1>
       ) : (
         <div>
-          <select onChange={onSelect}>
-            {coins.map((coin) => (
-              <option key={coin.id} value={[coin.quotes.USD.price, coin.name]}>
-                {coin.name} ({coin.symbol}) : $
-                {coin.quotes.USD.price.toFixed(2)} USD
-              </option>
-            ))}
-          </select>
-          <input
-            className={styles.input}
-            onChange={onChange}
-            value={money}
-            type="number"
-            placeholder="Enter your budget($)"
-          />
-
-          <h5>
-            You can buy {money ? Math.floor(money / coinPrice) : 0} {coinName}
-          </h5>
+          {movies.map((movie) => (
+            <div key={movie.id}>
+              <img src={movie.medium_cover_image}></img>
+              <h2>{movie.title}</h2>
+              <p>{movie.summary}</p>
+              {movie.hasOwnProperty("genres") ? (
+                <ul>
+                  {movie.genres.map((g) => (
+                    <li key={g}>{g}</li>
+                  ))}
+                </ul>
+              ) : null}
+            </div>
+          ))}
         </div>
       )}
     </div>
